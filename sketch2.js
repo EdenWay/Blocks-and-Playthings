518ed3c5b423e1066a86cdf6734f78dac3690e75
EDEN <edenway@my.unt.edu>
2024-12-04 10:09:22 -0600

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,72 +1,86 @@
-// GIF and Sound Libraries
-let myGif;
-let startGif, flashGif, openGif, wrtntextGif, fulltxtGif, baseGif, slashRGif, slashLGif, gameoverGif;
-let cursorImage;
-let dragonHealth = 40;
-let maxHealth = 40;
-let dragonDead = false;
+import gifAnimation.Gif; //music by: David Renda and David Fesliyan
+import processing.sound.*;
 
-let startSound, start2Sound, bossFightSound, slashSound, endSound;
+Gif myGif;
+boolean flashPlaying = false;
+boolean openPlaying = false;
+boolean wrtntextPlaying = false;
+boolean fulltxtPlaying = false;
+boolean basePlaying = false;
+boolean slashToggle = false;  
+boolean gameOverPlaying = false;  
 
-function preload() {
- startGif = new Gif(this, "assets/start.GIF");
-flashGif = new Gif(this, "assets/flash.gif");
-openGif = new Gif(this, "assets/open.gif");
-wrtntextGif = new Gif(this, "assets/wrtntext.gif");
-fulltxtGif = new Gif(this, "assets/fulltxt.GIF");
-baseGif = new Gif(this, "assets/base.GIF");
-slashRGif = new Gif(this, "assets/slashR.GIF");
-slashLGif = new Gif(this, "assets/slashL.GIF");
-gameoverGif = new Gif(this, "assets/gameover.gif");
+Gif startGif, flashGif, openGif, wrtntextGif, fulltxtGif, baseGif, slashRGif, slashLGif, gameoverGif;
+PImage cursorImage;
+int newWidth = 1200;
+int newHeight = 1200;
 
-startSound = new SoundFile(this, "assets/start.mp3");
-start2Sound = new SoundFile(this, "assets/start2.mp3");
-bossFightSound = new SoundFile(this, "assets/bossfight.mp3");
-slashSound = new SoundFile(this, "assets/slash.mp3");
-endSound = new SoundFile(this, "assets/end.mp3");
+int dragonHealth = 40;  
+int maxHealth = 40;     
+boolean dragonDead = false;
 
-  // Load cursor image
-  cursorImage = loadImage("mouse.PNG");
-}
+SoundFile startSound, start2Sound, bossFightSound, slashSound, endSound;
 
-function setup() {
-  createCanvas(1920, 1100);
-  noCursor();
+void setup() {
+  size(1920, 1100, P2D); 
+  frameRate(60);
+  background(0);
+  
+  // Loading GIFs from the 'assets' folder
+  startGif = new Gif(this, "assets/start.GIF");
+  flashGif = new Gif(this, "assets/flash.gif");
+  openGif = new Gif(this, "assets/open.gif");
+  wrtntextGif = new Gif(this, "assets/wrtntext.gif");
+  fulltxtGif = new Gif(this, "assets/fulltxt.GIF");
+  baseGif = new Gif(this, "assets/base.GIF");
+  slashRGif = new Gif(this, "assets/slashR.GIF");
+  slashLGif = new Gif(this, "assets/slashL.GIF");
+  gameoverGif = new Gif(this, "assets/gameover.gif");
+
+  // Loading sounds from the 'assets' folder
+  startSound = new SoundFile(this, "assets/start.mp3");
+  start2Sound = new SoundFile(this, "assets/start2.mp3");
+  bossFightSound = new SoundFile(this, "assets/bossfight.mp3");
+  slashSound = new SoundFile(this, "assets/slash.mp3");  
+  endSound = new SoundFile(this, "assets/end.mp3");
 
   startSound.play();
   resetGame();
+  
+  cursorImage = loadImage("assets/mouse.PNG"); // cursor image from assets
+  cursorImage.resize(newWidth, newHeight);
+  noCursor();
 }
 
-function draw() {
-  if (!dragonDead) {
+void draw() {
+  if (!gameOverPlaying) {
     image(baseGif, 0, 0, width, height);
     drawHealthBar();
 
-    if (myGif !== baseGif) {
-      if (myGif === slashRGif || myGif === slashLGif) {
-        let xPos = (width - myGif.width) / 2;
-        let yPos = (height - myGif.height) / 2;
-        image(myGif, xPos, yPos);
+    if (myGif != baseGif) {
+      if (myGif == slashRGif || myGif == slashLGif) {
+        int xPos = (width - myGif.width) / 2;
+        int yPos = (height - myGif.height) / 2;
+        image(myGif, xPos, yPos); 
       } else {
-        image(myGif, 0, 0, width, height);
+        image(myGif, 0, 0, width, height); 
       }
     }
 
     image(cursorImage, mouseX - cursorImage.width / 2, mouseY - cursorImage.height / 2);
 
-    // Game logic
     if (!flashPlaying && !openPlaying && !wrtntextPlaying && !fulltxtPlaying && !basePlaying) {
       noFill();
       noStroke();
-      rect(815, 645, 285, 70);
+      rect(815, 645, 285, 70);  
     }
 
-    // Handle transitions between GIFs
     if (flashPlaying && !flashGif.isPlaying()) {
       openPlaying = true;
       myGif = openGif;
       myGif.play();
-      if (!bossFightSound.isPlaying()) {
+
+      if (bossFightSound != null && !bossFightSound.isPlaying()) {
         bossFightSound.loop();
       }
       flashPlaying = false;
@@ -75,14 +89,14 @@ function draw() {
     if (openPlaying && !openGif.isPlaying()) {
       wrtntextPlaying = true;
       myGif = wrtntextGif;
-      myGif.play();
+      myGif.play(); 
       openPlaying = false;
     }
 
     if (wrtntextPlaying && !wrtntextGif.isPlaying()) {
       fulltxtPlaying = true;
       myGif = fulltxtGif;
-      myGif.play();
+      myGif.play();  
       wrtntextPlaying = false;
     }
 
@@ -91,22 +105,22 @@ function draw() {
     }
 
     if (dragonDead) {
-      gameOver();
+      gameOver();  
     }
   } else {
-    image(gameoverGif, 0, 0, width, height);
+    image(gameoverGif, 0, 0, width, height);  
     image(cursorImage, mouseX - cursorImage.width / 2, mouseY - cursorImage.height / 2);
   }
 }
 
-function mousePressed() {
+void mousePressed() {
   if (gameOverPlaying) {
     if (mouseX > 780 && mouseX < 780 + 370 && mouseY > 280 && mouseY < 280 + 85) {
       resetGame();
     }
   } else {
     if (!flashPlaying && !openPlaying && !wrtntextPlaying && !fulltxtPlaying && !basePlaying &&
-      mouseX > 815 && mouseX < 815 + 285 && mouseY > 645 && mouseY < 645 + 70) {
+        mouseX > 815 && mouseX < 815 + 285 && mouseY > 645 && mouseY < 645 + 70) {
       if (startSound.isPlaying()) {
         startSound.stop();
       }
@@ -114,14 +128,14 @@ function mousePressed() {
 
       flashPlaying = true;
       myGif = flashGif;
-      myGif.play();
+      myGif.play();  
     }
-
+    
     if (fulltxtPlaying && mousePressed) {
       fulltxtPlaying = false;
       basePlaying = true;
       myGif = baseGif;
-      myGif.loop();
+      myGif.loop();  
     }
 
     if (basePlaying) {
@@ -132,45 +146,45 @@ function mousePressed() {
         myGif = slashLGif;
         myGif.play();
       }
-      slashToggle = !slashToggle;
+      slashToggle = !slashToggle;  
 
       dragonHealth--;
 
-      if (slashSound && !slashSound.isPlaying()) {
+      if (slashSound != null && !slashSound.isPlaying()) {
         slashSound.play();
       }
 
       if (dragonHealth <= 0) {
-        dragonDead = true;
+        dragonDead = true;  
       }
     }
   }
 }
 
-function drawHealthBar() {
+void drawHealthBar() {
   fill(0);
-  rect(367, 34, 395, 32);
+  rect(367, 34, 395, 32);  
   fill(255, 0, 0);
-  let healthWidth = map(dragonHealth, 0, maxHealth, 0, 395);
-  rect(367, 34, healthWidth, 32);
+  float healthWidth = map(dragonHealth, 0, maxHealth, 0, 395); 
+  rect(367, 34, healthWidth, 32);  
 }
 
-function gameOver() {
-  gameOverPlaying = true;
+void gameOver() {
+  gameOverPlaying = true;  
   myGif = gameoverGif;
-  myGif.loop();
+  myGif.loop();  
 
   if (bossFightSound.isPlaying()) {
     bossFightSound.stop();
   }
-
-  if (endSound && !endSound.isPlaying()) {
+  
+  if (endSound != null && !endSound.isPlaying()) {
     endSound.loop();
   }
 }
 
-function resetGame() {
-  dragonHealth = maxHealth;
+void resetGame() {
+  dragonHealth = maxHealth;  
   dragonDead = false;
   gameOverPlaying = false;
 
@@ -187,14 +201,13 @@ function resetGame() {
   if (bossFightSound.isPlaying()) {
     bossFightSound.stop();
   }
-
-  if (endSound && endSound.isPlaying()) {
+  
+  if (endSound != null && endSound.isPlaying()) {
     endSound.stop();
   }
 
-  if (startSound) {
+  if (startSound != null) {
     startSound.stop();
     startSound.play();
   }
 }
-
